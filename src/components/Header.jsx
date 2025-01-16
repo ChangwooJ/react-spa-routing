5dcf90d464b60f972bb3f4ec9db2cb8b9ce6722b
gogo1414 <wnstn6945@gmail.com>
2025-01-16 14:16:17 +0900

    feature: News API를 위한 fetchNews util 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,42 +2,44 @@ import styled from "styled-components";
 import { categoryList } from "../data/category.js";
 
 const HeaderContainer = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem;
 `;
 
 const Title = styled.h1`
-  font-size: 2rem;
-  margin-bottom: 1rem;
+    font-size: 2rem;
+    margin-bottom: 1rem;
 `;
 
 const TabContainer = styled.div`
-  display: flex;
-  gap: 0.5rem;
-  margin-bottom: 1rem;
+    display: flex;
+    gap: 0.5rem;
+    margin-bottom: 1rem;
 `;
 
 const Tab = styled.button`
-  background-color: ${({ active }) => (active ? '#007bff' : 'transparent')};
-  color: ${({ active }) => (active ? '#fff' : 'inherit')};
-  border: none;
-  padding: 0.5rem 1rem;
-  cursor: pointer;
-  border-radius: 20px;
-  transition: background-color 0.3s;
-
-  &:hover {
-    background-color: #007bff;
-    color: #fff;
-  }
+    background-color: ${({ active }) => (active ? '#007bff' : 'transparent')};
+    color: ${({ active }) => (active ? '#fff' : 'inherit')};
+    border: none;
+    padding: 0.5rem 1rem;
+    cursor: pointer;
+    border-radius: 20px;
+    transition: background-color 0.3s;
+    
+    &:hover {
+        background-color: #007bff;
+        color: #fff;
+    }
 `;
 
 const DarkModeButton = styled.button`
-  margin-top: 1rem;
-  padding: 0.5rem 1rem;
-  cursor: pointer;
+    padding: 0.1rem 0.3rem;
+    border: none;
+    border-radius: 20px;
+    cursor: pointer;
+    font-size: 11px;
 `;
 
 const Header = ({ isDarkMode, onToggleDarkMode }) => {
@@ -51,10 +53,10 @@ const Header = ({ isDarkMode, onToggleDarkMode }) => {
                         {category.name}
                     </Tab>
                 ))}
+                <DarkModeButton onClick={onToggleDarkMode}>
+                    {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+                </DarkModeButton>
             </TabContainer>
-            <DarkModeButton onClick={onToggleDarkMode}>
-                {isDarkMode ? 'Light Mode' : 'Dark Mode'}
-            </DarkModeButton>
         </HeaderContainer>
     )
 }
