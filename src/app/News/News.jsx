68cc321e42bff207e00465494f10d9abb87376d6
songhj10207@naver.com <songhj10207@naver.com>
2025-01-21 18:12:15 +0900

    refactor: NoArticle 컴포넌트 삭제

--- src/app/News/News.jsx
+++ src/app/News/News.jsx
@@ -2,11 +2,9 @@ import NewsMain from './NewsMain';
 import {
   LoadingMessage,
   ErrorMessage,
-  NoArticlesMessage,
 } from '../../components/News/NewsMessage/NewsMessage';
 
 export const News = Object.assign(NewsMain, {
   Loading: LoadingMessage,
   Error: ErrorMessage,
-  NoArticles: NoArticlesMessage,
 });
