478aa386f3409e7bb56f977c6da10c8a0d8d9561
songhj10207@naver.com <songhj10207@naver.com>
2025-01-21 16:37:23 +0900

    refactor: media query로 초기 다크모드 설정

--- src/providers/ThemeContext.jsx
+++ src/providers/ThemeContext.jsx
@@ -4,6 +4,10 @@ import { lightTheme, darkTheme } from '../theme/theme';
 const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    localStorage.setItem('theme', 'dark');
+  }
+
   const initTheme =
     localStorage.getItem('theme') === 'dark' ? darkTheme : lightTheme;
   const [theme, setTheme] = useState(initTheme ? initTheme : null);
